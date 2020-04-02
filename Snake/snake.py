d2c0b4c645eaf27dc3a0f4fa36fb12f48b4b511c
joeatron <63060723+joeatron@users.noreply.github.com>
2020-04-02 16:34:56 +0100

    Update snake.py

--- Snake/snake.py
+++ Snake/snake.py
@@ -1,22 +1,30 @@
-import pygame
+#-----------------
+import pygame 
 import random
-
+#-----------------
 pygame.init()
-
+#-----------------
+#screen resolotion 
+#-----------------
 display_width = 640
 display_height = 480
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('snake')
-
+#-----------------
+#colours and sprit hit box info
+#-----------------
 black = (0,0,0)
 white = (255,255,255)
-snake_width = 8
-
+snake_width = 8 # hit box info
+gameDisplay.fill(black)
+#-----------------
+#fps and events
+#-----------------
 clock = pygame.time.Clock()
 end = False
 #got = False
-gameDisplay.fill(black)
+
 #----------------
 #Sprites
 #----------------
@@ -33,28 +41,32 @@ def food(foodx,foody):
 #Main loop
 #----------------
 def game_loop():
+    #-----------------
+    #X,Y coranates
+    #-----------------
     x =  (display_width * 0.45)
     y = (display_height * 0.45)
     foodx = random.randint(0,80)*8
     foody = random.randint(0,60)*8
-
-
-    direction = 0
-    x_change = 0
+    direction = 0 # SNAKES starting dirtion (left)
+    x_change = 0 
     y_change = 0
-    snake_speed = 0
+    snake_speed = 0 # speed
     end = False
+    #-----------------
+    #main loop 
+    #-----------------
     while not end:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 end = True
-#----------------
-#Move
-#----------------
+                #----------------
+                #Move contols
+                #----------------
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     direction = 0
-                    #x_change = -8
+                    #x_change = -8 
                 elif event.key == pygame.K_RIGHT:
                     direction = 180
                     #x_change = 8
@@ -64,13 +76,17 @@ def game_loop():
                 elif event.key == pygame.K_DOWN:
                     direction = 270
                     #y_change = 8
-                    
+            #-----------------
+            #let go reset
+            #-----------------
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
                 elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     y_change = 0
-                    
+            #-----------------
+            #auto move
+            #-----------------
             if direction == 0:
                 x_change = -8
                 y_change = 0
@@ -83,7 +99,9 @@ def game_loop():
             elif direction == 270:
                 y_change = 8
                 x_change = 0
-
+            #-----------------
+            #failed wall atemped
+            #-----------------
             if x == 640:
                 end = True
             elif x == 0:
@@ -93,23 +111,23 @@ def game_loop():
             elif y == 0:
                 end = True
 
-#----------------
-#
-#----------------        
+        #----------------
+        #set up coradnate stuff
+        #----------------        
         x += x_change
         y += y_change       
         gameDisplay.fill(black)
         snake(x,y)
         food(foodx,foody)
-#----------------
-#Events
-#----------------
+        #----------------
+        #Food Event
+        #----------------
         if x == foodx and y == foody :
             foodx = random.randint(0,80)*8
             foody = random.randint(0,60)*8
-#----------------
-#Frame update
-#----------------                
+        #----------------
+        #Frame update
+        #----------------                
         pygame.display.update()
         clock.tick(30)
 #----------------
